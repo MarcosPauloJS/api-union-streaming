d4e09f953b09871e6cf3460101c5594b76b08ca7
MarcosPauloJS <marcospaulooj@gmail.com>
2020-04-11 19:39:53 -0300

    troca de conexão ao banco de dados

--- src/database/firebaseConfig.js
+++ src/database/firebaseConfig.js
@@ -5,8 +5,8 @@ const firebaseConfig = {
     projectId: "union-streaming",
     storageBucket: "union-streaming.appspot.com",
     messagingSenderId: "662555774184",
-    appId: "1:662555774184:web:906ac8520928e2f0864d5d",
-    measurementId: "G-ZZR8D8VJR1"
+    appId: "1:662555774184:web:962574c5c34f6436864d5d",
+    measurementId: "G-PNBM81B41V"
   };
 
 module.exports = firebaseConfig;
---
+++ src/database/firebaseConnection.js
@@ -0,0 +1,14 @@
+const firebaseConnection = require('../database/firebaseConnection');
+
+module.exports = {
+    async index(request, response){
+        console.log('here')
+        const services = await firebaseConnection.database().ref('/services/')
+        .once('value')
+        .then( async snapshot =>{
+            return snapshot.val();
+        })
+
+        response.json(services)
+    }
+} 
